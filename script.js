72fbc40995ab94fa4f25121a0defac5d8bd776b5
Andrea Jarich <glenstone93@yahoo.com>
2022-10-05 01:14:08 -0500

    Created a javascript file. Created an event listener that will created a new file from the text in the textarea and then download the file to your computer.

---
+++ script.js
@@ -0,0 +1,15 @@
+const textarea = document.querySelector('textarea'),
+fileNameInput = document.querySelector('.file-name input'),
+selectMenu = document.querySelector('.save-as select'),
+saveBtn = document.querySelector('.save-btn');
+
+saveBtn.addEventListener('click', () => {
+  const blob = new Blob([textarea.value], {type: selectMenu.value});
+  //URL.createObjectURL creates a url that represent passed object
+  const fileUrl = URL.createObjectURL(blob);
+  const link = document.createElement("a"); //creating <a> tag
+  link.download = fileNameInput.value; //passing filename as download
+  link.href = fileUrl; // passing fileUrl as href value of link
+  link.click(); // clicking link so the file download
+
+});
